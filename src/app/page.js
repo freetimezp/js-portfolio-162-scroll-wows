2a3816c76619fe3bb322bedb1b09a50081f821c3
freetimezp <kraftukrnet@ukr.net>
2024-11-25 21:47:05 +0200

    02. js-portfolio-162-scroll-wows. Main UI.

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,70 @@
+"use client";
+import { useEffect } from "react";
+import Link from "next/link";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ReactLenis } from "lenis/react";
 import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const generateRows = () => {
+    const rows = [];
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    for (let i = 1; i <= 3; i++) {
+      rows.push(
+        <div className="row" key={i}>
+          <div className="card card-left">
+            <img src={`/img-${2 * i - 1}.jpg`} alt="image slide" />
+          </div>
+          <div className="card card-right">
+            <img src={`/img-${2 * i}.jpg`} alt="image slide" />
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      );
+    }
+
+    return rows;
+  };
+
+  return (
+    <>
+      <ReactLenis root>
+        <section className="hero">
+          <div className="img">
+            <Image src="/logo.png" alt="logo" width={300} height={180} />
+          </div>
+        </section>
+
+        <section className="main">
+          <div className="main-content">
+            <div className="logo">
+              <Image src="/logo.png" alt="logo" width={300} height={180} />
+            </div>
+
+            <div className="copy">
+              <div className="line">
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+              <div className="line">
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+              <div className="line">
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+              </div>
+            </div>
+
+            <div className="btn">
+              <button>Get More</button>
+            </div>
+          </div>
+
+          {generateRows()}
+        </section>
+
+        <section className="footer">
+          <Link href="#">Link in Description</Link>
+        </section>
+      </ReactLenis>
+    </>
   );
 }
