69d0ca23f43c3d1cfd47f5c44ce223172fafd3cb
freetimezp <kraftukrnet@ukr.net>
2024-11-25 22:51:22 +0200

    03. js-portfolio-162-scroll-wows. GSAP animation.

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,82 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ReactLenis } from "lenis/react";
 import Image from "next/image";
 
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Home() {
+  useEffect(() => {
+    const scrollTriggerSettings = {
+      trigger: ".main",
+      start: "top 25%",
+      toggleActions: "play reverse play reverse",
+    };
+
+    const leftXValues = [-800, -900, -400];
+    const rightXValues = [800, 900, 400];
+    const leftRotationValues = [-30, -20, -35];
+    const rightRotationValues = [30, 20, 35];
+    const yValues = [100, -150, -400];
+
+    gsap.utils.toArray(".row").forEach((row, index) => {
+      const cardLeft = row.querySelector(".card-left");
+      const cardRight = row.querySelector(".card-right");
+
+      gsap.to(cardLeft, {
+        x: leftXValues[index],
+        scrollTrigger: {
+          trigger: ".main",
+          start: "top center",
+          end: "150% bottom",
+          scrub: true,
+          onUpdate: (self) => {
+            const progress = self.progress;
+
+            cardLeft.style.transform = `
+              translateX(${progress * leftXValues[index]}px) 
+              translateY(${progress * yValues[index]}px)
+              rotate(${progress * leftRotationValues[index]}deg)
+            `;
+
+            cardRight.style.transform = `
+              translateX(${progress * rightXValues[index]}px) 
+              translateY(${progress * yValues[index]}px)
+              rotate(${progress * rightRotationValues[index]}deg)
+            `;
+          }
+        }
+      });
+    });
+
+    gsap.to(".logo", {
+      scale: 1,
+      duration: 0.5,
+      ease: "power1.out",
+      scrollTrigger: scrollTriggerSettings,
+    });
+
+    gsap.to(".line p", {
+      y: 0,
+      stagger: 0.1,
+      duration: 0.5,
+      ease: "power1.out",
+      scrollTrigger: scrollTriggerSettings,
+    });
+
+    gsap.to("button", {
+      y: 0,
+      opacity: 1,
+      delay: 0.25,
+      duration: 0.5,
+      ease: "power1.out",
+      scrollTrigger: scrollTriggerSettings,
+    });
+
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, []);
+
   const generateRows = () => {
     const rows = [];
 
